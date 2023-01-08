910858f500ae71b6c42c77a077150bb1cf4d0d0c
Tianbo <tianbo@hey.com>
2023-01-08 11:53:00 -0800

    Intent

---
+++ app/src/main/java/io/tianb/geoquiz/CheatActivity.kt
@@ -0,0 +1,59 @@
+package io.tianb.geoquiz
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import io.tianb.geoquiz.databinding.ActivityCheatBinding
+
+private const val TAG = "CheatActivity"
+
+const val EXTRA_ANSWER_SHOWN = "io.tianb.qeoquiz.answer_shown"
+private const val EXTRA_ANSWER_IS_TRUE = "io.tianb.geoquiz.answer_is_true"
+
+class CheatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCheatBinding
+
+    private var answerIsTrue = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        binding.showAnswerButton.setOnClickListener {
+            var answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            binding.answerTextView.setText(answerText)
+            setAnswerShownResult(true)
+        }
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy()")
+    }
+
+    private fun setAnswerShownResult(isAnswerShown: Boolean) {
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK, data)
+    }
+}
--- app/src/main/java/io/tianb/geoquiz/MainActivity.kt
+++ app/src/main/java/io/tianb/geoquiz/MainActivity.kt
@@ -1,11 +1,14 @@
 package io.tianb.geoquiz
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import io.tianb.geoquiz.databinding.ActivityMainBinding
 
@@ -17,6 +20,15 @@ class MainActivity : AppCompatActivity() {
 
     private val quizViewModel: QuizViewModel by viewModels()
 
+    private val cheatLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        // handle the result
+        if (result.resultCode == Activity.RESULT_OK) {
+            quizViewModel.isCheater =
+                result.data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +51,16 @@ class MainActivity : AppCompatActivity() {
         binding.nextButton.setOnClickListener {
             quizViewModel.moveToNext()
             updateQuestion()
+        }
+
+        binding.cheatButton.setOnClickListener {
+            // start CheatActivity
+//            val intent = Intent(this, CheatActivity::class.java)
+//            startActivity(intent)
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+//            startActivity(intent)
+            cheatLauncher.launch(intent)
         }
 
         updateQuestion()
@@ -77,10 +99,16 @@ class MainActivity : AppCompatActivity() {
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = quizViewModel.currentQuestionAnswer
 
-        val messageResId = if (userAnswer === correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+//        val messageResId = if (userAnswer === correctAnswer) {
+//            R.string.correct_toast
+//        } else {
+//            R.string.incorrect_toast
+//        }
+
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
