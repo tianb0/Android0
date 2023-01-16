4016885b89e97ec4027cbaa3b89835d2f9998309
Tianbo <tianbo@hey.com>
2023-01-15 17:47:48 -0800

    compile, target, min version

--- app/src/main/java/io/tianb/geoquiz/MainActivity.kt
+++ app/src/main/java/io/tianb/geoquiz/MainActivity.kt
@@ -2,6 +2,9 @@ package io.tianb.geoquiz
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +13,7 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import io.tianb.geoquiz.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
@@ -64,6 +68,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            blurCheatButton()
+        }
     }
 
     override fun onStart() {
@@ -113,5 +121,15 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun blurCheatButton() {
+        val effect = RenderEffect.createBlurEffect(
+            10.0f,
+            10.0f,
+            Shader.TileMode.CLAMP
+        )
+        binding.cheatButton.setRenderEffect(effect)
     }
 }
